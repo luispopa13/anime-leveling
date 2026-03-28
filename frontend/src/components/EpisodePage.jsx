ff861cafd4f246c4a655ad9c80c16efd5c84b728
luispop13 <andreasluispopa@gmail.com>
2026-03-28 05:55:08 +0200

    fix: player height responsive

--- frontend/src/components/EpisodePage.jsx
+++ frontend/src/components/EpisodePage.jsx
@@ -160,7 +160,7 @@ const WatchModal = ({ animeSlug, animeTitle, episodeNumber, totalEpisodes, anime
                   top      : '-130px',          // taie header aniwatchtv
                   left     : 0,
                   width    : '100%',
-                  height   : 'calc(100% + 330px + 310px)', // extinde jos → barele ies din container
+                  height   : 'calc(100% + 30px + 310px)', // extinde jos → barele ies din container
                   border   : 'none',
                 }}
               />
